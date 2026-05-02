0af001d6a3e10949984d52ce140c09f92f7ae391
ltbratcher42TNTech <ltbratcher42@tntech.edu>
2026-05-01 21:58:36 -0500

    added electron and added .envexample format

---
+++ backend/electron/main.js
@@ -0,0 +1,27 @@
+// electron/main.js
+const { app, BrowserWindow } = require('electron')
+const { spawn } = require('child_process')
+const path = require('path')
+
+// Start the Express backend
+const objServer = spawn(process.execPath, [path.join(__dirname, '..', 'server.js')], {
+    cwd: path.join(__dirname, '..'),
+    stdio: 'inherit'
+})
+
+// Wait for backend then open window
+const fnCreateWindow = () => {
+    const objWindow = new BrowserWindow({ width: 1280, height: 900 })
+
+    // Give Express a moment to start, then load it
+    setTimeout(() => {
+        objWindow.loadURL('http://localhost:8000')
+    }, 1500)
+}
+
+app.whenReady().then(fnCreateWindow)
+
+// Kill the backend when app closes
+app.on('will-quit', () => {
+    objServer.kill()
+})
