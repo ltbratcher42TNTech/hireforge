3acfe1d32f4091cf1e3b7f2c5d7dbdc3ed2c8545
ltbratcher42TNTech <ltbratcher42@tntech.edu>
2026-06-13 20:09:48 -0500

    Altered db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,7 +3,9 @@
 
 const sqlite3 = require('sqlite3').verbose()
 
-const db = new sqlite3.Database('resume.db', (err) => {
+// AI Assisted: Recommended t make this path here absolute, I figured no harm in trying
+const path = require('path')
+const db = new sqlite3.Database(path.join(__dirname, '../resume.db'), (err) => {
     if (err) {
         console.error("Db connection error:", err.message)
     } else {
@@ -13,5 +15,8 @@ const db = new sqlite3.Database('resume.db', (err) => {
 
 // Keeping SQLite foreign key enforcement enabled for the app connection.
 db.run("PRAGMA foreign_keys = ON")
+
+// Enable write ahead whatever it's called, keeps concurrent users able to write to database
+db.run('PRAGMA journal_mode=WAL')
 
 module.exports = db
